dae39d76463556fe0749adec737df3bf6bd0a3fe
darkswat90 <darkswat90@mail.ru>
2019-05-20 22:07:17 +0300

    Задания выполнены

--- easy.py
+++ easy.py
@@ -1,34 +1,76 @@
-# Задача-1: поработайте с переменными, создайте несколько,
-# выведите на экран, запросите от пользователя и сохраните в переменную, выведите на экран
+# Задача-1:
+# Напишите скрипт, создающий директории dir_1 - dir_9 в папке,
+# из которой запущен данный скрипт.
+# И второй скрипт, удаляющий эти папки.
 
-name = input("Введите ваше имя: ")
-surname = input("Введите вашу фамилию: ")
-age = int(input("Введите ваш год рождения: "))
-height = int(input("Введите ваш рост: "))
+import os
+import shutil
 
-print("Вот полная информация о вас:","\nИмя:", name, "\nФамилия:", surname, "\nВозраст:", (2019 - age), "лет;", "\nРост:", height, "см;" )
-
-# Задача-2: Запросите от пользователя число, сохраните в переменную,
-# прибавьте к числу 2 и выведите результат на экран.
-# Если возникла ошибка, прочитайте ее, вспомните урок и постарайтесь устранить ошибку.
-
-
-a = int(input("Введите число: "))
-
-sum = a + 2
-
-print(sum)
-
-# Задача-3: Запросите у пользователя его возраст.
-# Если ему есть 18 лет, выведите: "Доступ разрешен",
-# иначе "Извините, пользование данным ресурсом только с 18 лет"
+def make_dir (name):
+    try:
+        os.makedirs(name)
+    except FileExistsError:
+        print('{} - такая папка уже есть'.format(name))
 
 
-age = int(input("Введите ваш год рождения: "))
+def remove_dir (name):
+    try:
+        os.removedirs(name)
+    except FileNotFoundError:
+        print('{} - не существует'.format(name))
 
-realAge = 2019 - age
 
-if realAge >= 18:
-    print("Доступ разрешен")
-else:
-    print("Извините, пользование данным ресурсом только с 18 лет")
+def start ():
+    user_answer =''
+    count_dirs = range(1, 10)
+
+    while user_answer != '3':
+
+        user_answer = input('Выберите пункт меню:\n'
+                       '1. Создать папки dir_1 - dir_9\n'
+                       '2. Удалить папки dir_1 - dir_9\n'
+                       '3. Выход\n')
+
+        if user_answer == '1':
+            for i in count_dirs:
+                i = str(i)
+                make_dir('dir_' + i)
+        elif user_answer == '2':
+            for i in count_dirs:
+                i = str(i)
+                remove_dir('dir_' + i)
+        elif user_answer =='3':
+            break
+
+start()
+
+
+# Задача-2:
+# Напишите скрипт, отображающий папки текущей директории.
+
+def list_dir ():
+    buffer = os.listdir()
+    print('Список файлов:')
+    for index, element in enumerate(buffer, start=1):
+        if os.path.isdir(element):
+            print('{}. {}'.format(index, element))
+
+
+list_dir()
+
+
+# Задача-3:
+# Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+
+
+def current_file_copy ():
+    name_file = os.path.realpath(__file__)
+    new_file = name_file +'.copy'
+    if os.path.isfile(new_file) != True:
+        shutil.copy(name_file, new_file)
+        return new_file + ' - создан'
+    else:
+        return 'Копия текущего файла уже есть'
+
+
+print(current_file_copy())
